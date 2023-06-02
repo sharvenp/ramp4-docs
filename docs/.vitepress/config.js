cd86f4f3185c5cd1ceb3f302889cdefdd53d1248
Sharven Prasad Dhanasekar <Sharven.Dhanasekar@ec.gc.ca>
2023-06-02 10:56:07 -0400

    update vitepress config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,7 @@
 export default {
-    title: 'RAMP4 Documentation',
+    title: 'Reusable Accessible Mapping Platform Documentation',
+    description: "Reusable Accessible Mapping Platform documentation",
+    lang: 'en-CA',
     base: '/ramp4-docs/',
     srcDir: '../docs',
     outDir: '../vite-docs',
