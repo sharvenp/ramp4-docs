127fa6c8a74969ba990a1745d3e52972508af14b
Sharven Prasad Dhanasekar <Sharven.Dhanasekar@ec.gc.ca>
2023-06-02 09:37:56 -0400

    configure vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,9 @@
+export default {
+    title: 'RAMP4 Documentation',
+    base: '/ramp4-docs/',
+    srcDir: '../docs',
+    outDir: '../vite-docs',
+    markdown: {
+      lineNumbers: true
+    }
+  }
